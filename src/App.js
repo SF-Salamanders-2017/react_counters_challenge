b5f6eb90c10b5be18b0ee8b224e3646dd2841f5f
Ally Seidl & Kenn Sugiyama <pair+AllyJo.KennSugiyama@devbootcamp.com>
2017-11-27 12:38:42 -0800

    display total for incrementing/decrementing numbers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import Counter from './Counter';
 import './App.css';
+import './Counter.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      counter1: 0,
+      counter2: 0,
+      counter3: 0,
+      count: 0
+    };
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.totalCount = this.totalCount.bind(this)
+  }
+
+  increment(counter, countBy, count) {
+      this.setState({ [counter]: count + countBy})
+    }
+
+    decrement(counter, countBy, count) {
+      this.setState({ [counter]: count - countBy})
+    }
+    totalCount() {
+      return (
+        this.state.counter1 + this.state.counter2 + this.state.counter3
+
+        )
+      }
+
   render() {
     return (
       <div className="page-center-frame">
-        <Counter myProp={1} />
-        <Counter myProp={2} />
-        <Counter myProp={3} />
+        <Counter
+          counter = "counter1"
+          countBy = {1}
+          count = {this.state.counter1}
+          increment = {this.increment}
+          decrement = {this.decrement}
+         />
+        <Counter
+          counter = "counter2"
+          countBy = {2}
+          count = {this.state.counter2}
+          increment = {this.increment}
+          decrement = {this.decrement}/>
+        <Counter
+          counter = "counter3"
+          countBy = {3}
+          count = {this.state.counter3}
+          increment = {this.increment}
+          decrement = {this.decrement} />
+
+        <div className="counter">
+          Total: {this.totalCount()}
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -2,31 +2,19 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
-    };
+  // constructor() {
+    // super();
+  //   this.state = {
+  //     count: 0
+    // };
 
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-
-  }
-
-  increment() {
-    this.setState({ count: this.state.count + this.props.myProp})
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - this.props.myProp})
-  }
 
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+</button>
-        <div className="count">{this.state.count}</div>
-        <button onClick={this.decrement}>-</button>
+        <button onClick={() => this.props.increment(this.props.counter, this.props.countBy, this.props.count)}>+</button>
+        <div className="count">{this.props.count}</div>
+        <button onClick={() => this.props.decrement(this.props.counter, this.props.countBy, this.props.count)}>-</button>
 
       </div>
     );
