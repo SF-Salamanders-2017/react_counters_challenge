803414ac3c9fc576b291b76923b2a944226a0b77
Kevin Cai <kevincai1979@gmail.com>
2017-11-28 17:43:07 -0800

    update the state to the app component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,54 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+        counters: [
+          {count: 0, increment: 1},
+          {count: 0, increment: 2},
+          {count: 0, increment: 3}
+      ]
+
+    };
+      // this.incrementOne = this.incrementOne.bind(this)
+      this.increment = this.increment.bind(this)
+  }
+
+  increment(index, increment) {
+    const counters = this.state.counters
+    counters[index].count += increment
+    this.setState({ counters } );
+  }
+
+  decrement(index, decrement) {
+    const counters = this.state.counters
+    counters[index].count -= decrement
+    this.setState({ counters } );
+  }
+
   render() {
+    console.log(this.state.counters);
+    console.log(this.state.counters[0]);
+    console.log(this.state.counters[0].count)
+    console.log(this.state.counters[1].increment)
+    console.log(Object.keys(this.state.counters[0])[0])
+
     return (
       <div className="page-center-frame">
-        <Counter count={1} />
-        <Counter count={2}/>
-        <Counter count={3} />
+        {this.state.counters.map((counter, index) => {
+          return (
+            <Counter
+            increment={() => this.increment(index, counter.increment)}
+            decrement={() => this.decrement(index, counter.increment)}
+            count={counter.count}
+            />
+          )
+          })}
+
+        <div>
+          Total:
+        </div>
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -2,30 +2,30 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
-    };
+  // constructor() {
+  //   super();
+    // this.state = {
+    //
+    // };
 
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-  }
+    // this.increment = this.increment.bind(this)
+    // this.decrement = this.decrement.bind(this)
+  // }
 
-  increment() {
-    this.setState({ count: this.state.count + this.props.count })
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - this.props.count})
-  }
+  // increment() {
+  //   this.setState({ count: this.state.count + this.props.count })
+  // }
+  //
+  // decrement() {
+  //   this.setState({ count: this.state.count - this.props.count})
+  // }
 
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+</button>
-        <div className="count">{this.state.count}</div>
-        <button onClick={this.decrement}>-</button>
+        <button onClick={this.props.increment}>+</button>
+        <div className="count">{this.props.count}</div>
+        <button onClick={this.props.decrement}>-</button>
       </div>
     );
   }
