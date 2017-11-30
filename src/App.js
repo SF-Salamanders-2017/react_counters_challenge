c7f94d172a836d454f64cd7b97ea41b00c4f5c6d
Joseph Lesca <josephlesca@hotmail.com>
2017-11-30 12:23:27 -0800

    counters incrase by 1, 2, and 3.  Total is kept.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,52 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counters: [
+        {count: 0, countBy: 1},
+        {count: 0, countBy: 2},
+        {count: 0, countBy: 3}
+      ]
+    };
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.total = this.total.bind(this)
+  }
+
+  increment(index) {
+    let counters = this.state.counters
+    counters[index].count += counters[index].countBy;
+    debugger
+    this.setState({counters})
+  }
+
+  decrement(index) {
+    let counters = this.state.counters
+    counters[index].count -= counters[index].countBy;
+    debugger
+    this.setState({counters})
+  }
+
+  total() {
+    return this.state.counters.reduce((sum, counter) => sum + counter.count, 0);
+  }
+
   render() {
     return (
       <div className="page-center-frame">
-        <Counter myCount={1} />
-        <Counter myCount={2} />
-        <Counter myCount={3} />
+        {(this.state.counters)
+             .map((counter, index) =>
+              <Counter increment={() => this.increment(index)}
+              decrement={() => this.decrement(index)} count={counter.count} />
+        )}
+
+        <div>
+          <span>Total {this.total()}</span>
+        </div>
       </div>
     );
   }
