4256ea6ca55168b1e45e020568339d158681601f
Kevin Cai <kevincai1979@gmail.com>
2017-11-29 23:23:53 -0800

    add code so that can add and remove counters

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ class App extends React.Component {
           {count: 0, countBy: 1},
           {count: 0, countBy: 2},
           {count: 0, countBy: 3}
-      ]
+      ],
+      newCountBy: 1
     };
       this.increment = this.increment.bind(this)
+      this.decrement = this.decrement.bind(this)
       this.total = this.total.bind(this)
+      this.setNewCountBy = this.setNewCountBy.bind(this)
+      this.add = this.add.bind(this)
+      this.remove = this.remove.bind(this)
   }
 
   increment(index, countBy) {
@@ -34,22 +39,55 @@ class App extends React.Component {
     , 0)
   }
 
+  add(event) {
+    event.preventDefault()
+
+    let counters = this.state.counters
+    counters.push({
+      count: 0,
+      countBy: this.state.newCountBy
+    })
+    this.setState({ counters, newCountBy: 1})
+  }
+
+  remove(index) {
+    let counters = this.state.counters
+    counters.splice(index, 1)
+    this.setState({ counters })
+  }
+
+  setNewCountBy(event){
+    this.setState({ newCountBy: parseInt(event.target.value) || 1 })
+  }
+
   render() {
     return (
       <div className="page-center-frame">
         {this.state.counters.map((counter, index) => {
           return (
             <Counter
+            key={index}
             increment={() => this.increment(index, counter.countBy)}
             decrement={() => this.decrement(index, counter.countBy)}
             count={counter.count}
+            remove={() => this.remove(index)}
             />
-          )})
+          )
+          })
         }
 
-        <div>
+        <p>
           Total: {this.total()}
-        </div>
+        </p>
+
+        <form>
+          <input
+            type = 'text'
+            onChange={this.setNewCountBy}
+            value={this.state.setNewCountBy}
+          />
+          <button onClick={this.add}>Add Counter</button>
+        </form>
       </div>
     );
   }
